e5e04815ad1ddbf63c1b66a425a83899506735df
NeeshuSharmaa <neeshusharmaa@gmail.com>
2022-10-27 21:17:35 +0530

    create component: hero

---
+++ airbnb/src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function HeroSection() {
+  return (
+    <div>
+      <section className="hero-sec">
+        <img className="hero-img" src="images/photo-grid.png" alt="" />
+      </section>
+      <section className="content-sec">
+        <h1 className="heading">Online Experiences</h1>
+        <p>
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </section>
+    </div>
+  );
+}
