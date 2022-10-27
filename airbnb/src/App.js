59c8c0ef1e5dd0005d20d8432e2771d46dcb0b59
NeeshuSharmaa <neeshusharmaa@gmail.com>
2022-10-28 00:34:03 +0530

    create component: card

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -2,12 +2,41 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Card from './components/Card';
+import data from "./data.js";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
+      <div className='card-container'>
+        <Card 
+          img="katie-zaferes.png"
+          title="Life Lessons with Katie Zaferes"
+          rating="5.0"
+          count={6}
+          country="USA"
+          price={136}
+        />
+        <Card
+          img="wedding-photography.png"
+          title="Learn Wedding Photography"
+          rating="5.0"
+          count={30}
+          country="USA"
+          price={125}
+        />
+        <Card
+          img="mountain-bike.png"
+          title="Group Mountain Biking"
+          rating="4.8"
+          count={2}
+          country="USA"
+          price={50}
+        />
+      </div>
+      
     </div>
   );
 }
---
+++ airbnb/src/components/Card.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+
+export default function Card(props){
+    return(
+        <div className="card">
+            <img className="card-image" src={`images/${props.img}`} alt="" />
+            <span className="ratings-sec">
+                <img className="star-img" src="images/star.png" alt="" />
+                <span className="rating">{props.rating} </span>
+                <span className="count">({props.count}) ● </span>
+                <span className="country">{props.country}</span>
+            </span>
+            <span className="title">{props.title}</span>
+            <div>
+            <span className="price">From {props.price}</span> <span className="normal-span-text">/person</span>
+            </div>
+            
+        </div>
+    )
+}
+
+// export default function Card(){
+//     return(
+//         <div className="card">
+//             <img className="card-image" src="" alt="" />
+//             <span className="ratings-sec">
+//                 <img className="star-img" src="" alt="" />
+//                 <span className="rating"></span>
+//                 <span className="country"></span>
+//             </span>
+//             <p>description</p>
+//             <span className="price">$    /person</span>
+//         </div>
+//     )
+// }
